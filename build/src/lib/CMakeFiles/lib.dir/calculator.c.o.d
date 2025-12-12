e5b0094441cb43e84e49a21583b3080b33a10e08
Cyrilnkl <cyrilnlk5@gmail.com>
2025-12-12 14:01:35 +0100

    Remove build folder and add to gitignore

--- build/src/lib/CMakeFiles/lib.dir/calculator.c.o.d
+++
@@ -1,3 +0,0 @@
-src/lib/CMakeFiles/lib.dir/calculator.c.o: \
-  /Users/nakhla/Desktop/efrei/conteneur/Atelier_CMAKE/src/lib/calculator.c \
-  /Users/nakhla/Desktop/efrei/conteneur/Atelier_CMAKE/src/lib/./../../include/calculator.h
