a6912f0bbaf4de097d4a41a7ca417adcf8d284a2
Cyrilnkl <cyrilnlk5@gmail.com>
2025-12-12 13:59:35 +0100

    exercise done

---
+++ build/src/lib/CMakeFiles/lib.dir/calculator.c.o.d
@@ -0,0 +1,3 @@
+src/lib/CMakeFiles/lib.dir/calculator.c.o: \
+  /Users/nakhla/Desktop/efrei/conteneur/Atelier_CMAKE/src/lib/calculator.c \
+  /Users/nakhla/Desktop/efrei/conteneur/Atelier_CMAKE/src/lib/./../../include/calculator.h
